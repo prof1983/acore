0d07e94ad5d9dc24d818f0b4a60c759d35e1b88c
Prof1983 <prof1983@yandex.ru>
2013-04-15 14:54:38 +0600

    Updated

--- Delphi7/ACore32.dpr
+++ Delphi7/ACore32.dpr
@@ -2,14 +2,14 @@
 @Abstract ACore32 library
 @Author Prof1983 <prof1983@ya.ru>
 @Created 20.08.2009
-@LastMod 04.04.2013
+@LastMod 15.04.2013
 }
 library ACore32;
 
 {$R *.res}
 
 uses
-  ACoreBoot in '..\ACoreBoot.pas';
+  ACoreMain in '..\ACoreMain.pas';
 
 exports
   ACore_Boot,
