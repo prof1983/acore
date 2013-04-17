b98b785d59c7ceeb3f29ffefd3306e47fdafe1a3
Prof1983 <prof1983@yandex.ru>
2013-04-17 13:08:57 +0600

    Added ACore source files (Delphi7) version 0.6.1

---
+++ ACoreMain.pas
@@ -0,0 +1,72 @@
+{**
+@Abstract ACore
+@Author Prof1983 <prof1983@ya.ru>
+@Created 30.10.2009
+@LastMod 17.04.2013
+}
+unit ACoreMain;
+
+interface
+
+{$I A.inc}
+
+uses
+  ABase,
+  ADataMod,
+  APluginsExp,
+  APluginsMod,
+  ARuntimeExp,
+  ARuntimeMain,
+  ASettingsMod,
+  AStringsExp,
+  ASystemExp,
+  ASystemMod,
+  AUiMod,
+  AUtilsMod;
+
+// --- ACore ---
+
+function ACore_Boot(): AError; stdcall;
+
+function ACore_Fin(): AError; stdcall;
+
+function ACore_Init(): AError; stdcall;
+
+function ACore_Run(): AError; stdcall;
+
+implementation
+
+// --- ACore ---
+
+function ACore_Boot(): AError;
+begin
+  ADataMod_Boot();
+  APluginsMod_Boot();
+  ASettingsMod_Boot();
+  ASystemMod_Boot();
+  AUiMod_Boot();
+  AUtilsMod_Boot();
+  Result := 0;
+end;
+
+function ACore_Fin(): AError;
+begin
+  Result := ARuntime_Fin();
+end;
+
+function ACore_Init(): AError;
+begin
+  ARuntime_Init();
+  ASystemMod_Init();
+  AUiMod_Init();
+  APluginsMod_Init();
+  Result := 0;
+end;
+
+function ACore_Run(): AError;
+begin
+  Result := ARuntime_Run();
+end;
+
+end.
+ 
---
+++ Delphi7/ACore32.dpr
@@ -0,0 +1,22 @@
+{**
+@Abstract ACore32 library
+@Author Prof1983 <prof1983@ya.ru>
+@Created 20.08.2009
+@LastMod 17.04.2013
+}
+library ACore32;
+
+{$R *.res}
+
+uses
+  ACoreMain in '..\ACoreMain.pas';
+
+exports
+  ACore_Boot,
+  ACore_Fin,
+  ACore_Init,
+  ACore_Run;
+
+begin
+end.
+ 
